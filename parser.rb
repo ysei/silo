846e11f756ba8cfc2c4974041cfb7d23883f7886
Gene Doyel <gene.doyel@cph.org>
2013-12-01 21:41:02 -0600

    Refactored into more atomic rules

--- parser.rb
+++ parser.rb
@@ -2,50 +2,58 @@ require 'parslet'
 
 module Silo
   class Parser < Parslet::Parser
-
-    rule(:space) { match(' ') }
+    rule(:space) { match('\s') }
     rule(:space?) { space.maybe }
-
-    rule(:digit) { match('[0-9]') }
-    rule(:integer) { digit.repeat(1) }
-    rule(:float) { digit.repeat(1) >> str('.') >> digit.repeat(1) }
-    rule(:number) { integer | float }
-
     rule(:quote) { str('"') }
-    rule(:string) { quote >> (quote.absent? >> any).repeat >> quote }
+    rule(:comma) { str(",") >> space? }
+    rule(:lparen) { str('(') >> space? }
+    rule(:rparen) { space? >> str(')') }
+    rule(:lbracket) { str('[') >> space? }
+    rule(:rbracket) { space? >> str(']') }
+    rule(:lbrace) { str('{') >> space? }
+    rule(:rbrace) { space? >> str('}') }
+    rule(:period) { str('.') }
+    rule(:colon) { str(':') >> space? }
+    rule(:equals) { str('=') }
+    rule(:minus) { str('-') }
+    rule(:bang) { str('!') }
 
     rule(:truth) { str('true') }
     rule(:falsehood) { str('false') }
     rule(:nope) { str('nope') }
+    rule(:digit) { match('[0-9]') }
+
+    rule(:integer) { digit.repeat(1) }
+    rule(:float) { digit.repeat(1) >> period >> digit.repeat(1) }
+
+    rule(:number) { integer | float }
+    rule(:string) { quote >> (quote.absent? >> any).repeat >> quote }
 
     rule(:literal) { number | string | truth | falsehood | nope }
 
     rule(:identifier) { match('[a-zA-Z_]') >> match('\w').repeat }
-    rule(:variable) { identifier >> (str('.') >> identifier).maybe }
 
-    rule(:argument_delimiter) { str(",") >> space? }
-    rule(:arguments) { expression >> (argument_delimiter >> expression).repeat }
+    rule(:variable) { identifier >> (period >> identifier).maybe }
 
-    rule(:method_call) { variable >> str('(') >> arguments.maybe >> str(')') }
+    rule(:arguments) { term >> (comma >> term).repeat }
+    rule(:method_call) { variable >> lparen >> arguments.maybe >> rparen }
 
-    rule(:assignment_operator) { space? >> str('=') >> space? }
-    rule(:assignment) { variable >> assignment_operator >> term }
+    rule(:assignment) { variable >> space? >> equals >> space? >> term }
 
-    rule(:hash_assignment_operator) { str(':') >> space? }
-    rule(:hash_assignment) { identifier >> hash_assignment_operator >> expression}
-    rule(:hash) { str('{') >> (hash_assignment >> (argument_delimiter >> hash_assignment).repeat).maybe >> str('}') }
+    rule(:hash_assignment) { identifier >> colon >> expression }
+    rule(:hash) { lbrace >> (hash_assignment >> (comma >> hash_assignment).repeat).maybe >> rbrace }
 
-    rule(:array) { str('[') >> arguments.maybe >> str(']') }
+    rule(:array) { lbracket >> arguments.maybe >> rbracket }
 
     rule(:term) { literal | method_call | variable | assignment | hash | array }
 
-    rule(:arithmetic_operator) { space? >> match('[-+*/%]') >> space? }
-    rule(:comparison_operator) { space? >> match('[><]') >> str('=').maybe >> space? }
+    rule(:arithmetic_operator) { match('[-+*/%]') }
+    rule(:comparison_operator) { match('[><]') >> equals.maybe }
     rule(:equality_operator) { str('isnt') | str('is') | str('==') | str('!=') }
-    rule(:compound_assignment_operator) { match('[-+*/]') >> str('=') }
+    rule(:compound_assignment_operator) { match('[-+*/]') >> equals }
     rule(:operator) { compound_assignment_operator | arithmetic_operator | comparison_operator | equality_operator }
 
-    rule(:unary_operation) { match('[-!]') >> term }
+    rule(:unary_operation) { (minus | bang) >> term }
     rule(:binary_operation) { term >> space? >> operator >> space? >> term }
     rule(:operation) { unary_operation | binary_operation }
 
